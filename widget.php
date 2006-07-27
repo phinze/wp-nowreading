de0aee8905a2095c1a292adca2710232febb44fd
rob <rob@341b472c-1117-0410-a120-85441c4cc759>
2006-07-27 12:02:48 +0000

    Try and delete the widget file ourselves, and throw an error asking the user to do it manually if we can't.

--- widget.php
+++ widget.php
@@ -1,53 +1,55 @@
 <?php
 
-if ( !function_exists('nrWidgetInit') ) {
-	function nrWidgetInit() {
-		if ( !function_exists('register_sidebar_widget') )
-			return;
-		
-		function nrWidget($args) {
-			extract($args);
-			
-			$options = get_option('nowReadingWidget');
-			$title = $options['title'];
-			
-			echo $before_widget . $before_title . $title . $after_title;
-			if( !defined('NOW_READING_VERSION') || floatval(NOW_READING_VERSION) < 4.2 ) {
-				echo "<p>You don't appear to have the Now Reading plugin installed, or have an old version; you'll need to install or upgrade before this widget can display your data.</p>";
-			} else {
-				nr_load_template('sidebar.php');
-			}
-			echo $after_widget;
-		}
-		
-		function nrWidgetControl() {
-			$options = get_option('nowReadingWidget');
-			
-			if ( !is_array($options) )
-				$options = array('title' => 'Now Reading');
-				
-			if ( $_POST['nowReadingSubmit'] ) {
-				$options['title'] = htmlentities(stripslashes($_POST['nowReadingTitle']));
-				update_option('nowReadingWidget', $options);
-			}
-			
-			$title = htmlspecialchars($options['title'], ENT_QUOTES);
-			
-			echo '
-				<p style="text-align:right;">
-					<label for="nowReadingTitle">Title: 
-						<input style="width: 200px;" id="nowReadingTitle" name="nowReadingTitle" type="text" value="'.$title.'" />
-					</label>
-				</p>
-			<input type="hidden" id="nowReadingSubmit" name="nowReadingSubmit" value="1" />
-			';
-		}
+$widget_file = ABSPATH . '/wp-content/plugins/widgets/now-reading.php';
+if ( ( function_exists('nrWidgetInit') || file_exists($widget_file) ) && !@unlink($widget_file) )
+	die("Please delete your <code>wp-content/plugins/widgets/now-reading.php</code> file!");
 
-		register_sidebar_widget('Now Reading', 'nrWidget');
-		register_widget_control('Now Reading', 'nrWidgetControl', 300, 100);
+function nrWidgetInit() {
+	if ( !function_exists('register_sidebar_widget') )
+		return;
+	
+	function nrWidget($args) {
+		extract($args);
+		
+		$options = get_option('nowReadingWidget');
+		$title = $options['title'];
+		
+		echo $before_widget . $before_title . $title . $after_title;
+		if( !defined('NOW_READING_VERSION') || floatval(NOW_READING_VERSION) < 4.2 ) {
+			echo "<p>You don't appear to have the Now Reading plugin installed, or have an old version; you'll need to install or upgrade before this widget can display your data.</p>";
+		} else {
+			nr_load_template('sidebar.php');
+		}
+		echo $after_widget;
+	}
+	
+	function nrWidgetControl() {
+		$options = get_option('nowReadingWidget');
+		
+		if ( !is_array($options) )
+			$options = array('title' => 'Now Reading');
+			
+		if ( $_POST['nowReadingSubmit'] ) {
+			$options['title'] = htmlentities(stripslashes($_POST['nowReadingTitle']));
+			update_option('nowReadingWidget', $options);
+		}
+		
+		$title = htmlspecialchars($options['title'], ENT_QUOTES);
+		
+		echo '
+			<p style="text-align:right;">
+				<label for="nowReadingTitle">Title: 
+					<input style="width: 200px;" id="nowReadingTitle" name="nowReadingTitle" type="text" value="'.$title.'" />
+				</label>
+			</p>
+		<input type="hidden" id="nowReadingSubmit" name="nowReadingSubmit" value="1" />
+		';
 	}
 
-	add_action('plugins_loaded', 'nrWidgetInit');
+	register_sidebar_widget('Now Reading', 'nrWidget');
+	register_widget_control('Now Reading', 'nrWidgetControl', 300, 100);
 }
+
+add_action('plugins_loaded', 'nrWidgetInit');
 
 ?>

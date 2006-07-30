5456258fdfc50bd519b94a591097fdc278cd4d4a
rob <rob@341b472c-1117-0410-a120-85441c4cc759>
2006-07-30 16:21:12 +0000

    Deactivate the old widget too, thanks Denis de Bernardy.

--- widget.php
+++ widget.php
@@ -1,5 +1,15 @@
 <?php
 
+// De-activate and attempt to delete the old widget.
+$active_plugins = get_settings('active_plugins');
+foreach ( (array) $active_plugins as $key => $plugin ) {
+	if ( $plugin == 'widgets/now-reading.php' ) {
+		unset($active_plugins[$key]);
+		sort($active_plugins);
+		update_option('active_plugins', $active_plugins);
+		break;
+	}
+}
 $widget_file = ABSPATH . '/wp-content/plugins/widgets/now-reading.php';
 if ( file_exists($widget_file) ) {
 	@chmod($widget_file, 0666);
